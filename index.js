1f9fea06bffc7beca0a7c70fc557c3ec074969df
Aran Froggatt <38727065+LordWunderfrog@users.noreply.github.com>
2019-01-20 23:00:02 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 class Node{
-	constructor(i, val, css){
-		plantPot.innerHTML+='<div class="node '+css+'" id='+i+'>'+val+'</div>';
-		this.children = [];
+	constructor(num, id){
+		this.num = num;
+		this.left = null;
+		this.right = null;
+		this.id = id;
+	}
+}
+
+class Tree{
+	constructor(){
+		this.parent = null;
+	}
+	
+	addLeaf(id){
+		
+		var num = Math.round(Math.random()*100);
+		var id = id;
+		var leaf = new Node(num, id);
+		
+		if(this.parent === null){
+			this.parent = leaf;
+			this.renderLeaf('root', leaf.num, leaf.id);
+		}else{
+			this.sortLeaf(this.parent, leaf);
+		}	
+	}
+	
+	sortLeaf(parent, leaf){
+		if(leaf.num < parent.num){
+			if(parent.left === null){
+				parent.left = leaf;
+				this.renderLeaf('left', leaf.num, leaf.id, parent.id);
+			}else{
+				this.sortLeaf(parent.left, leaf);
+			}
+		}else{
+			if(parent.right === null){
+				parent.right = leaf;
+				this.renderLeaf('right', leaf.num, leaf.id, parent.id);
+			}else{
+				this.sortLeaf(parent.right, leaf);
+			}
+		}
+	}	
+	
+	renderLeaf(css, num, id, parentId){
+		if(css!== 'root'){
+			plantPot = document.getElementById(css+'-'+parentId);
+		}
+		plantPot.innerHTML+=
+		'<div class="node '+css+'" id="'+id+'">'+num+'</div><ul class="halfCon"><li class="leftBranch" id="left-'+id+'"></li><li class="rightBranch" id="right-'+id+'"></li></ul>';
+		
 	}
 }
 
@@ -14,10 +63,11 @@ function GenTree(leaves){
 	}
 	
 	plantPot.innerHTML = '';
-	generation=1;
 	
-	for(i=0; i<leaves; i++){
-		leaf = new Node(i);
+	var sapling = new Tree();
+		
+	for(var counter=0;counter<leaves; counter++){
+		sapling.addLeaf(counter);
 	}
-	
-}
+}	
+
